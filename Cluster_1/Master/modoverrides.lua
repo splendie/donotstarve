dc1386aa8a9aa6574f698038200e88755f1fd334
Indri <indri@ruangguru.com>
2020-07-20 19:46:19 +0700

    First commit

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,14 @@
+return {
+  ["workshop-739170164"]={
+    configuration_options={
+      ["Blue Gem"]=0,
+      ["Cut Stone"]=1,
+      ["Gold Nugget"]=1,
+      Log=0,
+      ["Purple Gem"]=0,
+      ["Red Gem"]=0,
+      Science=2 
+    },
+    enabled=true 
+  } 
+}
---
+++ Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,27 @@
+return {
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=0,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-739170164"]={
+    configuration_options={
+      ["Blue Gem"]=0,
+      ["Cut Stone"]=1,
+      ["Gold Nugget"]=1,
+      Log=0,
+      ["Purple Gem"]=0,
+      ["Red Gem"]=0,
+      Science=2 
+    },
+    enabled=true 
+  },
+  ["workshop-753844771"]={ configuration_options={ gf="normal" }, enabled=true } 
+}
